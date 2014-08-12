55ee5054739d998f5d35f192632fe3c9d5ec2138
Mark Moulton <random-npc@hotmail.com>
2014-08-12 20:53:03 -0300

    Initial boss setup for Doom, prebehavior.

--- Assets/Scripts/Doom/DoomScript.cs
+++ Assets/Scripts/Doom/DoomScript.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class DoomScript : MonoBehaviour {
 
+	public Transform bossHealthBar;
+	public static Animator healthanim;
+	public static Animator anim;
+
+	public float refiretimer = 2f;
+	public float refiretimermax = 2f;
+
+	public Transform playerLocation;
+	public Transform doomSprite;	
+
 	// Use this for initialization
 	void Start () {
+		healthanim = bossHealthBar.GetComponent<Animator>();
+		healthanim.SetFloat ("bossHealth",EnemyHealth.currentHealth);
+		anim = doomSprite.GetComponent<Animator>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		healthanim.SetFloat("bossHealth",EnemyHealth.currentHealth);
+
 	}
 }
