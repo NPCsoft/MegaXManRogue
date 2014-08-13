0cd52991650c4474bb1f64cf7dde5c435d034be4
Mark Moulton <random-npc@hotmail.com>
2014-08-12 22:37:45 -0300

    Started Doom behavior

--- Assets/Scripts/Doom/DoomScript.cs
+++ Assets/Scripts/Doom/DoomScript.cs
@@ -12,12 +12,23 @@ public class DoomScript : MonoBehaviour {
 
 	public Transform playerLocation;
 	public Transform doomSprite;	
+	public Transform doomPosition;
+
+	public float vertSpeed = 5f;
+	public float moveSpeed = 5f;
+	public bool jumping = false;
+	public bool falling = false;
+	public bool moveLeft = false;
+	public bool moveRight = false;
+	public bool lastDirectionLeft = false;
+
 
 	// Use this for initialization
 	void Start () {
 		healthanim = bossHealthBar.GetComponent<Animator>();
 		healthanim.SetFloat ("bossHealth",EnemyHealth.currentHealth);
 		anim = doomSprite.GetComponent<Animator>();
+		Invoke ("PinkGround",3f);
 	
 	}
 	
@@ -25,5 +36,80 @@ public class DoomScript : MonoBehaviour {
 	void Update () {
 		healthanim.SetFloat("bossHealth",EnemyHealth.currentHealth);
 
+		if (jumping)
+		{
+			transform.Translate (Vector3.up * vertSpeed * Time.deltaTime);
+		}	
+
+		if (falling)
+		{
+			transform.Translate (Vector3.up * -vertSpeed * Time.deltaTime);
+		}
+
+		if (moveLeft)
+		{
+			transform.Translate (Vector3.right * -moveSpeed * Time.deltaTime);
+		}	
+
+		if (moveRight)
+		{
+			transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+		}
+
+	}
+
+	void PinkGround() 
+	{
+		anim.Play ("PinkGround");
+		Invoke ("JumpToMid",1f);
+	}
+
+	void JumpToMid()
+	{
+		jumping = true;
+		anim.Play ("ForwardJump");
+		if (doomPosition.localPosition.x > 0f)
+		{
+			moveLeft = true;
+			lastDirectionLeft = true;
+		}
+		else
+		{
+			moveRight = true;
+			lastDirectionLeft = false;
+		}
+		Invoke ("AirPink",1f);
+	}
+
+	void AirPink()
+	{
+		jumping = false;
+		moveLeft = false;
+		moveRight = false;
+		anim.Play ("AirPink");
+		Invoke ("ContinueFall",1f);
+	}
+
+	void ContinueFall()
+	{
+		falling = true;
+		if (lastDirectionLeft)
+			moveLeft = true;
+		else
+			moveRight = true;
+
+		Invoke ("GroundBeam",1f);
+	}
+
+	void GroundBeam()
+	{
+		if (doomPosition.localPosition.x > playerLocation.localPosition.x)
+		{
+			doomSprite.localScale = new Vector3 (-1,1,1);
+		}
+		else
+		{
+			doomSprite.localScale = new Vector3 (1,1,1);
+		}
 	}
 }
