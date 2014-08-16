fb822fa5874e615e5f7e8209b7683ecbb9c91927
Mark Moulton <random-npc@hotmail.com>
2014-08-16 01:28:49 -0300

    Completed menus, credits, stage select, etc.

---
+++ Assets/Scripts/Game Infrastructure/HighlightSelection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightSelection : MonoBehaviour {
+
+	public GameObject highlight;
+
+	void OnMouseOver()
+	{
+		highlight.renderer.enabled = true;
+	}
+
+	void OnMouseExit()
+	{
+		highlight.renderer.enabled = false;
+	}
+}
