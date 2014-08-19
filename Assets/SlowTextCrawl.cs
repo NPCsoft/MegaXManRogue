926ac80c1adbaea7239036f27c361235c5a6b766
Mark Moulton <random-npc@hotmail.com>
2014-08-19 08:39:45 -0300

    added win screen, game is fully playable!

---
+++ Assets/SlowTextCrawl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowTextCrawl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < 0f)
+		{
+			transform.Translate (Vector2.up * Time.deltaTime);
+		}
+		if (gameObject.transform.position.y >= 0f)
+		{
+			if (Input.anyKeyDown)
+				Application.LoadLevel ("MainMenu");
+		}
+	}
+}
