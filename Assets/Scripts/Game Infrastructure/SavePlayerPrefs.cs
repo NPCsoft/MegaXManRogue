6b2174ea81114ae1070de5c92a0859bd3d2788d3
Mark Moulton <random-npc@hotmail.com>
2014-08-21 00:02:28 -0300

    0.92 added save function on stage select screen to fix loss of playerprefs data

---
+++ Assets/Scripts/Game Infrastructure/SavePlayerPrefs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavePlayerPrefs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.Save();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
