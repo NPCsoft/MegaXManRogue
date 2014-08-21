11db179b1be8bf0539a666c9b0c927a1e6ab8e91
Mark Moulton <random-npc@hotmail.com>
2014-08-20 21:25:53 -0300

    small fix to drill claw hit box, fix to story stage display.

--- Assets/Scripts/Game Infrastructure/StoryLevelSelect.cs
+++ Assets/Scripts/Game Infrastructure/StoryLevelSelect.cs
@@ -7,20 +7,11 @@ public class StoryLevelSelect: MonoBehaviour {
 	public string levelName2;
 	public Texture mistertexture;
 	public GameObject questionmark;
+	public Color doomGreen;
 
 
 	void Start()
 	{
-		if (PlayerPrefs.GetInt ("MagnetoLevel") == 1 && PlayerPrefs.GetInt ("JuggernautLevel") == 1 &&
-		    PlayerPrefs.GetInt ("VenomLevel") == 1)
-			myMaterial.shader = Shader.Find ("Unlit/Texture");
-		else
-		{
-			myMaterial.SetTexture (0, null);
-			myMaterial.shader = Shader.Find ("Diffuse");
-			myMaterial.SetColor ("_Color", Color.gray);
-		}
-
 		if (PlayerPrefs.GetInt ("MagnetoLevel") == 1 && PlayerPrefs.GetInt ("JuggernautLevel") == 1 &&
 		    PlayerPrefs.GetInt ("VenomLevel") == 1)
 		{
@@ -28,14 +19,21 @@ public class StoryLevelSelect: MonoBehaviour {
 			{
 				questionmark.SetActive (false);
 				myMaterial.SetTexture (0, mistertexture);
+				myMaterial.shader = Shader.Find ("Unlit/Texture");
 			}
 			else if (PlayerPrefs.GetInt ("MisterSinisterLevel") == 1)
 			{
 				questionmark.SetActive (true);
 				myMaterial.SetTexture (0, null);
 				myMaterial.shader = Shader.Find ("Sprites/Default");
-				myMaterial.SetColor ("_TintColor", Color.green);
+				myMaterial.SetColor ("_Color", doomGreen);
 			}
+		}
+		else
+		{
+			myMaterial.SetTexture (0, null);
+			myMaterial.shader = Shader.Find ("Diffuse");
+			myMaterial.SetColor ("_Color", Color.gray);
 		}
 	}
 
