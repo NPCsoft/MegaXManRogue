5cb8b6f1160d41bac87e12d800526e2803a223d1
Mark Moulton <random-npc@hotmail.com>
2014-08-21 21:43:16 -0300

    New Game Plus implemented

---
+++ Assets/Scripts/Game Infrastructure/LoadLevelOnClickNGP.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnClickNGP : MonoBehaviour {
+
+	public string levelName;
+
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown (0))
+			Application.LoadLevel(levelName);
+	}
+}
