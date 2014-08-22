b69e9e72c1c5ed947662bfbdb38ec412d6916eb8
Mark Moulton <random-npc@hotmail.com>
2014-08-21 22:11:27 -0300

    fixed continue after NGP going to normal stage select

---
+++ Assets/Scripts/Game Infrastructure/GameButtonGoContinue.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameButtonGoContinue : MonoBehaviour {
+
+	public string levelname;
+	public string levelname2;
+	public GameObject selectup = null;
+	public GameObject selectdown = null;
+	public GameObject selectleft = null;
+	public GameObject selectright = null;
+	public float controllerdelay = 1f;
+	// Use this for initialization
+	void OnEnable () {
+		controllerdelay = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		controllerdelay += Time.deltaTime;
+		if (controllerdelay > 0.2f){
+		if (Input.GetButtonDown ("Dash") || Input.GetButtonDown ("Jump") || Input.GetButtonDown("Attack"))
+			{
+				if (PlayerPrefs.GetInt("NGP") == 1)
+					Application.LoadLevel (levelname2);
+				else
+					Application.LoadLevel (levelname);
+			}
+		if (selectup != null)
+		{
+			if (Input.GetAxis ("Vertical") > 0f && Input.GetAxis ("Horizontal") == 0f)
+			{
+				selectup.SetActive (true);
+			}
+		}
+
+		if (selectdown != null)
+		{
+				if (Input.GetAxis ("Vertical") < 0f && Input.GetAxis ("Horizontal") == 0f)
+			{
+				selectdown.SetActive (true);
+			}
+		}
+
+		if (selectright != null)
+		{
+				if (Input.GetAxis ("Horizontal") > 0f && Input.GetAxis ("Vertical") == 0f)
+			{
+				selectright.SetActive (true);
+			}
+		}
+
+		if (selectleft != null)
+		{
+				if (Input.GetAxis ("Horizontal") < 0f && Input.GetAxis ("Vertical") == 0f)
+			{
+				selectleft.SetActive (true);
+			}
+		}
+
+		if (selectup != null && selectup.activeSelf || selectdown != null && selectdown.activeSelf || 
+		    selectright != null && selectright.activeSelf || selectleft != null && selectleft.activeSelf)
+			gameObject.SetActive (false);
+	}
+	}
+}
---
+++ Assets/Scripts/Game Infrastructure/LoadContinueOnClickNGP.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadContinueOnClickNGP : MonoBehaviour {
+	public string levelName;
+	public string levelName2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			if (PlayerPrefs.GetInt("NGP") == 1)
+				Application.LoadLevel(levelName2);
+			else
+				Application.LoadLevel(levelName);
+		}
+	}
+}
